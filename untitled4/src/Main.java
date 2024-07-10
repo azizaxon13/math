5b9fe8aef7299113fc4c203c5abe2797669ef2a0
Dostonbek Qurolboyev <101505223+dostondevop@users.noreply.github.com>
2024-07-10 18:19:59 +0500

    Update Main.java

--- untitled4/src/Main.java
+++ untitled4/src/Main.java
@@ -6,7 +6,28 @@ public class Main {
     }
 
     public static void findPrimes(int n) {
+        int count = 0;
+        int num = 2;
+        while (count < n) {
+            if (isPrime(num)) {
+                System.out.print(num + " ");
+                count++;
+            }
+            num++;
+        }
+        System.out.println();
+    }
 
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 
 
