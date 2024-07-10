8efcf748b6e8db28dca61a0a4e546283efbb35bd
azizaxon13 <erkinbaevaaziza.13.com@gmail.com>
2024-07-10 17:12:00 +0500

    untitled4 added

---
+++ untitled4/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        findPrimes(13);
+        findEkub(200, 113);
+        findEkuk(24, 13);
+    }
+
+    public static void findPrimes(int n) {
+
+
+    }
+
+
+    public static void findEkub(int n, int k) {
+
+    }
+
+    public static void findEkuk(int n, int k) {
+
+    }
+}
