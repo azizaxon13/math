03f076d45ea4d26f7a4ed84cf431615c0fdb75e6
Dostonbek Qurolboyev <101505223+dostondevop@users.noreply.github.com>
2024-07-10 18:18:19 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -32,7 +32,14 @@ public class Main {
 
 
     public static void findEkub(int n, int k) {
-
+        while (n != k) {
+            if (n > k) {
+                n = n - k;
+            } else {
+                k = k - n;
+            }
+        }
+        System.out.println(n);
     }
 
     public static void findEkuk(int n, int k) {
